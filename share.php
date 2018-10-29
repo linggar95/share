f04973721a89c4465439a5684e24714d582aeaf3
linggar95 <44567693+linggar95@users.noreply.github.com>
2018-10-29 16:29:02 +0700

    Update share.php

--- share.php
+++ share.php
@@ -1,15 +1,15 @@
 <?php
-$mawar = $_GET["score"];
-$scorex = explode(":", $mawar);
+$score = $_GET["mawar"];
+$scorex = explode(":", $score);
 $highscore = $scorex[0];
 ?>
 <html>
 <head>
 <title>The Lost Queen HighScore</title>
-<meta property="og:title" content="Highscore on The Lost Queen is <?echo $mawar;?>!"/>
+<meta property="og:title" content="Highscore on The Lost Queen is <?echo $score;?>!"/>
 <!--<meta property="og:image" content="http://weebe.nl/jellojumper/jello.png"/>-->
 <meta property="og:site_name" content="The Lost Queen"/>
-<meta property="og:description" content="<?echo $mawar;?> is mine new highscore on The Lost Queen How far do you think you can get? Try it for free!"/> 
+<meta property="og:description" content="<?echo $score;?> is mine new highscore on The Lost Queen How far do you think you can get? Try it for free!"/> 
 </head>
 <body>
 <!--<meta http-equiv="refresh" content="0;URL=http://www.yoursite.com" /-->
