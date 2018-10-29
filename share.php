2a65677ad6706e9d0871b5b0c9e81ac1a6250f24
linggar95 <44567693+linggar95@users.noreply.github.com>
2018-10-29 15:54:40 +0700

    Add files via upload

---
+++ share.php
@@ -0,0 +1,17 @@
+<?php
+$mawar = $_GET["score"];
+$scorex = explode(":", $mawar);
+$highscore = $scorex[0];
+?>
+<html>
+<head>
+<title>The Lost Queen HighScore</title>
+<meta property="og:title" content="Highscore on The Lost Queen is <?echo $mawar;?>!"/>
+<!--<meta property="og:image" content="http://weebe.nl/jellojumper/jello.png"/>-->
+<meta property="og:site_name" content="The Lost Queen"/>
+<meta property="og:description" content="<?echo $mawar;?> is mine new highscore on The Lost Queen How far do you think you can get? Try it for free!"/> 
+</head>
+<body>
+<!--<meta http-equiv="refresh" content="0;URL=http://www.yoursite.com" /-->
+</body>
+</html>
